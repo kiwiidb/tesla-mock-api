f68bedede4e63309b1c27e3474a4533706579040
ext.kwinten.de.backer <ext.kwinten.de.backer@sofico.be>
2020-05-03 12:46:33 +0200

    fix go

--- api/data.go
+++ api/data.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/sirupsen/logrus"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	data, _ := ioutil.ReadFile("./mock_data.json")
+	data, err := ioutil.ReadFile("./mock_data.json")
+	if err != nil {
+		logrus.WithError(err).Error("error reading file")
+	}
 	fmt.Fprintf(w, string(data))
 }
